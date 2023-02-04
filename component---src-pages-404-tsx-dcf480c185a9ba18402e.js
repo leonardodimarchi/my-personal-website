2b2799982015e710de6b25d003a7e351305a582d
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2023-02-03 21:18:54 -0300

    Updates

---
+++ component---src-pages-404-tsx-dcf480c185a9ba18402e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_leonardodimarchi_my_personal_website=self.webpackChunk_leonardodimarchi_my_personal_website||[]).push([[218],{2513:function(e,n,l){l.r(n);var t=l(7294),a=l(6920);n.default=e=>t.createElement(a.Z,null,t.createElement("h1",null,"404 - Page Not Found"),t.createElement("p",null,"Unfortunately we couldn't find what you were looking for :("))}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-dcf480c185a9ba18402e.js.map
